58847b5ddb4ad56046c02c1ce15f26c6044b9d3c
elviec <34024536+elviec@users.noreply.github.com>
2020-11-22 15:57:27 +1300

    partners promote items

---
+++ promote.js
@@ -0,0 +1,134 @@
+function craftSteward() {
+  const servantHat = document.getElementById('servantHat').value;
+  const servantRobe = document.getElementById('servantRobe').value;
+  const servantBelt = document.getElementById('servantBelt').value;
+
+  const craftedStewardHat = Math.trunc(servantHat / 3);
+  const craftedStewardRobe = Math.trunc(servantRobe / 3);
+  const craftedStewardBelt = Math.trunc(servantBelt / 3);
+
+  let stewardHat = Number(document.getElementById('stewardHat').value);
+  let stewardRobe = Number(document.getElementById('stewardRobe').value);
+  let stewardBelt = Number(document.getElementById('stewardBelt').value);
+
+  document.getElementById('stewardHat').value = stewardHat + craftedStewardHat;
+  document.getElementById('stewardRobe').value = stewardRobe + craftedStewardRobe;
+  document.getElementById('stewardBelt').value = stewardBelt + craftedStewardBelt;
+}
+
+function craftCurator() {
+  const stewardHat = document.getElementById('stewardHat').value;
+  const stewardRobe = document.getElementById('stewardRobe').value;
+  const stewardBelt = document.getElementById('stewardBelt').value;
+
+  const craftedCuratorHat = Math.trunc(stewardHat / 3);
+  const craftedCuratorRobe = Math.trunc(stewardRobe / 3);
+  const craftedCuratorBelt = Math.trunc(stewardBelt / 3);
+
+  let curatorHat = Number(document.getElementById('curatorHat').value);
+  let curatorRobe = Number(document.getElementById('curatorRobe').value);
+  let curatorBelt = Number(document.getElementById('curatorBelt').value);
+
+  document.getElementById('curatorHat').value = curatorHat + craftedCuratorHat;
+  document.getElementById('curatorRobe').value = curatorRobe + craftedCuratorRobe;
+  document.getElementById('curatorBelt').value = curatorBelt + craftedCuratorBelt;
+}
+
+function craftDeputy() {
+  const curatorHat = document.getElementById('curatorHat').value;
+  const curatorRobe = document.getElementById('curatorRobe').value;
+  const curatorBelt = document.getElementById('curatorBelt').value;
+
+  const craftedDeputyHat = Math.trunc(curatorHat / 4);
+  const craftedDeputyRobe = Math.trunc(curatorRobe / 4);
+  const craftedDeputyBelt = Math.trunc(curatorBelt / 4);
+
+  let deputyHat = Number(document.getElementById('deputyHat').value);
+  let deputyRobe = Number(document.getElementById('deputyRobe').value);
+  let deputyBelt = Number(document.getElementById('deputyBelt').value);
+
+  document.getElementById('deputyHat').value = deputyHat + craftedDeputyHat;
+  document.getElementById('deputyRobe').value = deputyRobe + craftedDeputyRobe;
+  document.getElementById('deputyBelt').value = deputyBelt + craftedDeputyBelt;
+}
+
+function craftChief() {
+  const deputyHat = document.getElementById('deputyHat').value;
+  const deputyRobe = document.getElementById('deputyRobe').value;
+  const deputyBelt = document.getElementById('deputyBelt').value;
+
+  const craftedChiefHat = Math.trunc(deputyHat / 5);
+  const craftedChiefRobe = Math.trunc(deputyRobe / 5);
+  const craftedChiefBelt = Math.trunc(deputyBelt / 5);
+
+  let chiefHat = Number(document.getElementById('chiefHat').value);
+  let chiefRobe = Number(document.getElementById('chiefRobe').value);
+  let chiefBelt = Number(document.getElementById('chiefBelt').value);
+
+  document.getElementById('chiefHat').value = chiefHat + craftedChiefHat;
+  document.getElementById('chiefRobe').value = chiefRobe + craftedChiefRobe;
+  document.getElementById('chiefBelt').value = chiefBelt + craftedChiefBelt;
+}
+
+function craftArchChief() {
+  const chiefHat = document.getElementById('chiefHat').value;
+  const chiefRobe = document.getElementById('chiefRobe').value;
+  const chiefBelt = document.getElementById('chiefBelt').value;
+
+  const craftedArchiefHat = Math.trunc(chiefHat / 5);
+  const craftedArchiefRobe = Math.trunc(chiefRobe / 5);
+  const craftedArchiefBelt = Math.trunc(chiefBelt / 5);
+
+  let archiefHat = Number(document.getElementById('archiefHat').value);
+  let archiefRobe = Number(document.getElementById('archiefRobe').value);
+  let archiefBelt = Number(document.getElementById('archiefBelt').value);
+
+  document.getElementById('archiefHat').value = archiefHat + craftedArchiefHat;
+  document.getElementById('archiefRobe').value = archiefRobe + craftedArchiefRobe;
+  document.getElementById('archiefBelt').value = archiefBelt + craftedArchiefBelt;
+}
+
+function resetServant() {
+  document.getElementById('servantHat').value = '';
+  document.getElementById('servantRobe').value = '';
+  document.getElementById('servantBelt').value = '';
+}
+
+function resetSteward() {
+  document.getElementById('stewardHat').value = '';
+  document.getElementById('stewardRobe').value = '';
+  document.getElementById('stewardBelt').value = '';
+}
+
+function resetCurator() {
+  document.getElementById('curatorHat').value = '';
+  document.getElementById('curatorRobe').value = '';
+  document.getElementById('curatorBelt').value = '';
+}
+
+function resetDeputy() {
+  document.getElementById('deputyHat').value = '';
+  document.getElementById('deputyRobe').value = '';
+  document.getElementById('deputyBelt').value = '';
+}
+
+function resetChief() {
+  document.getElementById('chiefHat').value = '';
+  document.getElementById('chiefRobe').value = '';
+  document.getElementById('chiefBelt').value = '';
+}
+
+function resetArchief() {
+  document.getElementById('archiefHat').value = '';
+  document.getElementById('archiefRobe').value = '';
+  document.getElementById('archiefBelt').value = '';
+}
+
+function resetAll() {
+  resetServant();
+  resetSteward();
+  resetCurator();
+  resetDeputy();
+  resetChief();
+  resetArchief();
+}
